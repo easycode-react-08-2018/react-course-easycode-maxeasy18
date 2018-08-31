fd190ddbf565ad8fc0e2c01430cafc406f0cbace
maxeasy18 <maxym.marchenko@gmail.com>
2018-08-31 18:38:52 +0300

    lesson-2

---
+++ lesson-2/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+
+module.exports = {
+	entry : path.resolve('./src/main.js'),
+	output: {
+		filename: 'bundle.js',
+		publicPath: '/'
+	},
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /.js$/,
+				loader: 'babel-loader'
+			}
+		]
+
+	}
+}
